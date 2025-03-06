93db86636f02bf238a18d7f1685639cc429ec4d5
= <=>
2025-03-06 21:52:21 +0100

    Switch all controller methods to ResponseEntity for better status code management

--- src/main/java/com/kulbit/spring/rest/controller/BookContoller.java
+++ src/main/java/com/kulbit/spring/rest/controller/BookContoller.java
@@ -3,6 +3,8 @@ package com.kulbit.spring.rest.controller;
 import com.kulbit.spring.rest.entity.Book;
 import com.kulbit.spring.rest.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,18 +25,38 @@ public class BookContoller {
     }
 
     @GetMapping("/books/{id}")
-    public Book getBookById(@PathVariable int id) {
-        return bookService.getBookById(id);
+    public ResponseEntity<Book> getBookById(@PathVariable int id) {
+        Book book = bookService.getBookById(id);
+        if (book == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(book);
     }
 
     @PostMapping("/books")
-    public Book addBook(@RequestBody Book book) {
+    public ResponseEntity<Book> addBook(@RequestBody Book book) {
         bookService.saveBook(book);
-        return book;
+        return ResponseEntity.status(HttpStatus.CREATED).body(book);
     }
+
     @PutMapping("/books")
-    public Book updateBook(@RequestBody Book book) {
+    public ResponseEntity<Book> updateBook(@RequestBody Book book) {
+        Book bookToUpdate = bookService.getBookById(book.getId());
+        if (bookToUpdate == null) {
+            return ResponseEntity.notFound().build();
+        }
         bookService.saveBook(book);
-        return book;
+
+        return ResponseEntity.ok(book);
+    }
+
+    @DeleteMapping("/books/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable int id) {
+        Book bookToDelete = bookService.getBookById(id);
+        if (bookToDelete == null) {
+            return ResponseEntity.notFound().build();
+        }
+        bookService.deleteBook(id);
+        return ResponseEntity.noContent().build();
     }
 }
