f9500cbd172f85e4022489d56e9397642fa901b4
= <=>
2025-03-06 21:33:19 +0100

    Add @Service annotation to BookServiceImpl

--- src/main/java/com/kulbit/spring/rest/service/BookServiceImpl.java
+++ src/main/java/com/kulbit/spring/rest/service/BookServiceImpl.java
@@ -2,11 +2,13 @@ package com.kulbit.spring.rest.service;
 
 import com.kulbit.spring.rest.dao.BookDAO;
 import com.kulbit.spring.rest.entity.Book;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
+@Service
 public class BookServiceImpl implements BookService {
 
     BookDAO bookDAO;
