c04ebf8b7cff011654ab7eb040348e0a8c2af813
= <=>
2025-03-06 21:14:01 +0100

    Add Hibernate configuration with HikariCP for DataSource and transaction management

--- src/main/java/com/kulbit/spring/rest/configuration/MyConfig.java
+++ src/main/java/com/kulbit/spring/rest/configuration/MyConfig.java
@@ -1,13 +1,54 @@
 package com.kulbit.spring.rest.configuration;
 
+import com.fasterxml.jackson.databind.annotation.JsonAppend;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @ComponentScan(basePackages = "com.kulbit.spring.rest")
 @EnableWebMvc
 @EnableTransactionManagement
 public class MyConfig {
+
+    @Bean
+    public DataSource getDataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName("com.mysql.jdbc.Driver");
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/my_db?useSSL=false&serverTimezone=UTC");
+        config.setUsername("bestuser");
+        config.setPassword("bestuser");
+        return new HikariDataSource(config);
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(getDataSource());
+        sessionFactory.setPackagesToScan("com.kulbit.spring.rest.entity");
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
+        hibernateProperties.setProperty("hibernate.show_sql", "true");
+        sessionFactory.setHibernateProperties(hibernateProperties);
+
+        return sessionFactory;
+    }
+
+    public HibernateTransactionManager transactionManager() {
+
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory().getObject());
+
+        return transactionManager;
+    }
+
 }
