821083224b1810b0e5cb54c0693d6475bbcdc008
= <=>
2025-03-06 21:37:00 +0100

    Add GetMapping methods for Books.

--- src/main/java/com/kulbit/spring/rest/controller/BookContoller.java
+++ src/main/java/com/kulbit/spring/rest/controller/BookContoller.java
@@ -1,9 +1,11 @@
 package com.kulbit.spring.rest.controller;
 
+import com.kulbit.spring.rest.entity.Book;
 import com.kulbit.spring.rest.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -13,5 +15,15 @@ public class BookContoller {
 
     public BookContoller(@Autowired BookService bookService) {
         this.bookService = bookService;
+    }
+
+    @GetMapping("/books")
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/books/{id}")
+    public Book getBookById(@PathVariable int id) {
+        return bookService.getBookById(id);
     }
 }
